09b4073f06c5cacf17764ad7a1453cf043b69b55
Thijs <thijs.coremans@outlook.com>
2022-03-13 21:37:04 +0100

    wrap error response in ResponseEntity

--- rest/src/main/kotlin/com/mediagenix/api/rest/exception/ApiAdvice.kt
+++ rest/src/main/kotlin/com/mediagenix/api/rest/exception/ApiAdvice.kt
@@ -2,6 +2,8 @@ package com.mediagenix.api.rest.exception
 
 import com.mediagenix.api.core.exception.EntityNotFoundException
 import com.mediagenix.api.rest.dto.ErrorResponseDto
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
@@ -9,5 +11,5 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 class ApiAdvice {
 
     @ExceptionHandler(EntityNotFoundException::class)
-    fun handleEntityNotFoundException(e: EntityNotFoundException) = ErrorResponseDto("Requested entity has not been found")
+    fun handleEntityNotFoundException(e: EntityNotFoundException)= ResponseEntity(ErrorResponseDto("Requested entity has not been found"), HttpStatus.NOT_FOUND)
 }
