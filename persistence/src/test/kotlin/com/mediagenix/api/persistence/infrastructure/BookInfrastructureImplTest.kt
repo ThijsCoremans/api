10fd500b0ce4af0b21ad8ec039396022e650d8be
Thijs <thijs.coremans@outlook.com>
2022-03-14 10:42:45 +0100

    add unit tests for InfrastructureImpls

---
+++ persistence/src/test/kotlin/com/mediagenix/api/persistence/infrastructure/BookInfrastructureImplTest.kt
@@ -0,0 +1,40 @@
+package com.mediagenix.api.persistence.infrastructure
+
+import com.mediagenix.api.core.exception.BookNotFoundException
+import com.mediagenix.api.core.infrastructure.BookInfrastructure
+import com.mediagenix.api.core.model.Book
+import com.mediagenix.api.persistence.mapper.BookEntityMapper
+import com.mediagenix.api.persistence.repository.BookRepository
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import java.util.*
+
+internal class BookInfrastructureImplTest {
+
+    private val bookRepository: BookRepository = mockk()
+    private val bookEntityMapper: BookEntityMapper = mockk()
+    private val bookInfrastructure: BookInfrastructure = BookInfrastructureImpl(bookRepository, bookEntityMapper)
+
+    @Test
+    fun `getBookById throws exception if not found`() {
+        every { bookRepository.findById(any()) } returns Optional.empty()
+
+        assertThrows(BookNotFoundException::class.java) { bookInfrastructure.getBookById(1) }
+    }
+
+    @Test
+    fun `updateBook throws exception if not found`() {
+        every { bookRepository.existsById(any()) } returns false
+
+        assertThrows(BookNotFoundException::class.java) { bookInfrastructure.updateBook(Book(1, "title", "isbn", "author")) }
+    }
+
+    @Test
+    fun `deleteBookById throws exception if not found`() {
+        every { bookRepository.existsById(any()) } returns false
+
+        assertThrows(BookNotFoundException::class.java) { bookInfrastructure.deleteBookById(1) }
+    }
+}
---
+++ persistence/src/test/kotlin/com/mediagenix/api/persistence/infrastructure/CollectionInfrastructureImplTest.kt
@@ -0,0 +1,65 @@
+package com.mediagenix.api.persistence.infrastructure
+
+import com.mediagenix.api.core.exception.BookNotFoundException
+import com.mediagenix.api.core.exception.CollectionNotFoundException
+import com.mediagenix.api.core.infrastructure.CollectionInfrastructure
+import com.mediagenix.api.core.model.Collection
+import com.mediagenix.api.persistence.mapper.CollectionEntityMapper
+import com.mediagenix.api.persistence.repository.BookRepository
+import com.mediagenix.api.persistence.repository.CollectionRepository
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import java.util.*
+
+internal class CollectionInfrastructureImplTest {
+
+    private val collectionRepository: CollectionRepository = mockk()
+    private val bookRepository: BookRepository = mockk()
+    private val collectionEntityMapper: CollectionEntityMapper = mockk()
+    private val collectionInfrastructure: CollectionInfrastructure = CollectionInfrastructureImpl(collectionRepository, bookRepository, collectionEntityMapper)
+
+    @Test
+    fun `getCollectionById throws exception if not found`() {
+        every { collectionRepository.findById(any()) } returns Optional.empty()
+
+        assertThrows(CollectionNotFoundException::class.java) { collectionInfrastructure.getCollectionById(1) }
+    }
+
+    @Test
+    fun `updateCollection throws exception if not found`() {
+        every { collectionRepository.findById(any()) } returns Optional.empty()
+
+        assertThrows(CollectionNotFoundException::class.java) { collectionInfrastructure.updateCollection(Collection(1, "name", arrayListOf())) }
+    }
+
+    @Test
+    fun `deleteCollectionById throws exception if not found`() {
+        every { collectionRepository.existsById(any()) } returns false
+
+        assertThrows(CollectionNotFoundException::class.java) { collectionInfrastructure.deleteCollectionById(1) }
+    }
+
+    @Test
+    fun `addBookToCollection throws exception if collection not found`() {
+        every { collectionRepository.findById(any()) } returns Optional.empty()
+
+        assertThrows(CollectionNotFoundException::class.java) { collectionInfrastructure.addBookToCollection(1, 1) }
+    }
+
+    @Test
+    fun `addBookToCollection throws exception if book not found`() {
+        every { collectionRepository.findById(any()) } returns Optional.of(com.mediagenix.api.persistence.entity.Collection(1, "name", arrayListOf()))
+        every { bookRepository.findById(any()) } returns Optional.empty()
+
+        assertThrows(BookNotFoundException::class.java) { collectionInfrastructure.addBookToCollection(1, 1) }
+    }
+
+    @Test
+    fun `deleteBookFromCollection throws exception if collection not found`() {
+        every { collectionRepository.findById(any()) } returns Optional.empty()
+
+        assertThrows(CollectionNotFoundException::class.java) { collectionInfrastructure.deleteBookFromCollection(1, 1) }
+    }
+}
